fe36945dc642408112842a73eef2ce084c83ed5b
Luka Milanovic <mluka@student.ethz.ch>
2022-04-15 17:38:13 +0200

    cTCLS: remove global wildcard import, externalize regbus definitions

--- rtl/cTCLS_unit/cTCLS_unit.sv
+++ rtl/cTCLS_unit/cTCLS_unit.sv
@@ -12,15 +12,18 @@
 
 `include "register_interface/typedef.svh"
 // Peripheral communication signals
-import ctcls_manager_reg_pkg::* ;
-`REG_BUS_TYPEDEF_ALL(tcls, logic[31:0], logic[31:0], logic[3:0])
+
+
 
 module cTCLS_unit #(
-  parameter int unsigned InstrRdataWidth  = 32,
+  parameter int unsigned InstrRdataWidth = 32,
   parameter int unsigned NExtPerfCounters = 5,
-  parameter int unsigned DataWidth        = 32,
-  parameter int unsigned BEWidth          = 4
+  parameter int unsigned DataWidth = 32,
+  parameter int unsigned BEWidth = 4,
+  parameter type         tcls_req_t       = logic,
+  parameter type         tcls_rsp_t       = logic
 ) (
+  
   input logic                              clk_i,
   input logic                              rst_ni,
 
@@ -97,7 +100,10 @@ module cTCLS_unit #(
 
   // APU/SHARED_FPU not implemented
 );
+  
+   import ctcls_manager_reg_pkg::* ;
 
+  
    ctcls_manager_reg2hw_t reg2hw;
    ctcls_manager_hw2reg_t hw2reg;
    
