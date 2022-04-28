b3e1eccecdd661709336d5e2911b6b343bf46bf0
Luka Milanovic <mluka@student.ethz.ch>
2022-04-28 11:03:41 +0200

    created folder pulpissimo_tcls and moved copied/renamed the files from ctls_unit there

---
+++ rtl/pulpissimo_tcls/TCLS.h
@@ -0,0 +1,33 @@
+// Generated register defines for TCLS_manager
+
+#ifndef _TCLS_MANAGER_REG_DEFS_
+#define _TCLS_MANAGER_REG_DEFS_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// Register width
+#define TCLS_MANAGER_PARAM_REG_WIDTH 32
+
+// Stack Pointer storage register
+#define TCLS_MANAGER_SP_STORE_REG_OFFSET 0x0
+
+// Redundancy Mode configuration
+#define TCLS_MANAGER_MODE_REG_OFFSET 0x4
+#define TCLS_MANAGER_MODE_MODE_BIT 0
+#define TCLS_MANAGER_MODE_RESTORE_MODE_BIT 1
+
+// Mismatch counter of core 0
+#define TCLS_MANAGER_MISMATCHES_0_REG_OFFSET 0x8
+
+// Mismatch counter of core 1
+#define TCLS_MANAGER_MISMATCHES_1_REG_OFFSET 0xc
+
+// Mismatch counter of core 2
+#define TCLS_MANAGER_MISMATCHES_2_REG_OFFSET 0x10
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif  // _TCLS_MANAGER_REG_DEFS_
+// End generated register defines for TCLS_manager
