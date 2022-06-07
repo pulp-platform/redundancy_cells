ee678b8bf2f0b69d935d6117aba413264294f453
Michael Rogenmoser <michael@rogenmoser.us>
2022-06-07 11:20:42 +0200

    Add ECC manager scrubber trigger and logging

--- rtl/ecc_wrap/ecc_manager.sv
+++ rtl/ecc_wrap/ecc_manager.sv
@@ -21,10 +21,14 @@ module ecc_manager #(
   input  ecc_mgr_req_t        ecc_mgr_req,
   output ecc_mgr_rsp_t        ecc_mgr_rsp,
 
-  input  logic [NumBanks-1:0] bank_faults,
+  input  logic [NumBanks-1:0] bank_faults_i,
+  input  logic [NumBanks-1:0] scrub_fix_i,
+  output logic [NumBanks-1:0] scrub_trigger_o,
   output logic [NumBanks-1:0][38:0] test_write_mask_no
 );
   import ecc_manager_reg_pkg::*;
+
+  logic [NumBanks-1:0][31:0] counter_value;
 
   ecc_manager_reg2hw_t reg2hw;
   ecc_manager_hw2reg_t hw2reg;
@@ -43,11 +47,34 @@ module ecc_manager #(
   );
 
   for (genvar i = 0; i < NumBanks; i++) begin : gen_fault_increment
+    // Count ECC fixes on access
     assign hw2reg.mismatch_count[i].d = reg2hw.mismatch_count[i].q + 1;
-    assign hw2reg.mismatch_count[i].de = bank_faults[i];
+    assign hw2reg.mismatch_count[i].de = bank_faults_i[i];
 
+    // Count ECC fixes on scrub
+    assign hw2reg.scrub_fix_count[i].d = reg2hw.scrub_fix_count[i].q + 1;
+    assign hw2reg.scrub_fix_count[i].de = scrub_fix_i[i];
+
+    // Assign testing mask
     assign test_write_mask_no[i][31:0] = reg2hw.write_mask_data_n[i];
     assign test_write_mask_no[i][38:32] = reg2hw.write_mask_ecc_n[i];
+
+    // Instantiate scrub trigger counter
+    assign scrub_trigger_o[i] = counter_value[i] == reg2hw.scrub_interval.q;
+    counter #(
+      .WIDTH           ( 32   ),
+      .STICKY_OVERFLOW ( 1'b0 )
+    ) i_scrub_counter (
+      .clk_i,
+      .rst_ni,
+      .clear_i   ( reg2hw.scrub_interval.q == '0 ),
+      .en_i      ( reg2hw.scrub_interval.q != '0 ),
+      .load_i    ( counter_value[i] == reg2hw.scrub_interval.q ),
+      .down_i    ( 1'b0 ),
+      .d_i       ( '0 ),
+      .q_o       ( counter_value[i] ),
+      .overflow_o()
+    );
   end
 
 endmodule
