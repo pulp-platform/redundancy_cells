dba735f176bf5d322f55858aa70e874132081d0f
Michael Rogenmoser <michael@rogenmoser.us>
2023-01-30 19:42:29 +0100

    Update TMR signalling
    Update signal alignments

--- rtl/HMR/hmr_tmr_ctrl.sv
+++ rtl/HMR/hmr_tmr_ctrl.sv
@@ -25,7 +25,7 @@ module hmr_tmr_ctrl #(
   input  reg_req_t   reg_req_i,
   output reg_resp_t  reg_resp_o,
 
-  // CTRL from external (e.g. HMR ctr regs)
+  // CTRL from external (e.g. HMR ctrl regs)
   input  logic       tmr_enable_q_i,
   input  logic       tmr_enable_qe_i,
   input  logic       delay_resynch_q_i,
@@ -39,6 +39,7 @@ module hmr_tmr_ctrl #(
   
   // TMR control signals
   output logic       setback_o,
+  output logic       sw_resynch_req_o,
   output logic       grp_in_independent_o,
   output logic [2:0] tmr_incr_mismatches_o,
   input  logic       tmr_single_mismatch_i,
@@ -59,6 +60,7 @@ module hmr_tmr_ctrl #(
 
   assign setback_o = tmr_setback_q;
   assign grp_in_independent_o = tmr_red_mode_q == NON_TMR;
+  assign tmr_resynch_req_o = tmr_red_mode_q == TMR_UNLOAD;
 
   hmr_tmr_regs_reg_top #(
     .reg_req_t(reg_req_t),
@@ -90,60 +92,66 @@ module hmr_tmr_ctrl #(
   always_comb begin : proc_fsm
     tmr_setback_d = 1'b0;
     tmr_red_mode_d = tmr_red_mode_q;
-    tmr_incr_mismatches_o[0] = 1'b0;
-    tmr_incr_mismatches_o[1] = 1'b0;
-    tmr_incr_mismatches_o[2] = 1'b0;
+    tmr_incr_mismatches_o = '0;
 
     tmr_hw2reg.tmr_config.force_resynch.de  = force_resynch_qe_i;
 
-    // If forced execute resynchronization
-    if (tmr_red_mode_q == TMR_RUN && tmr_reg2hw.tmr_config.force_resynch.q) begin
-      tmr_hw2reg.tmr_config.force_resynch.de = 1'b1;
-      if (tmr_reg2hw.tmr_config.delay_resynch == 0) begin
-        tmr_red_mode_d = TMR_UNLOAD;
-        // TODO: buffer the restoration until delay_resynch is disabled
-      end
-    end
+    case (tmr_red_mode_q)
+      TMR_RUN: begin
+        // If forced execute resynchronization
+        if (tmr_reg2hw.tmr_config.force_resynch.q) begin
+          tmr_hw2reg.tmr_config.force_resynch.de = 1'b1;
+          if (tmr_reg2hw.tmr_config.delay_resynch == 0) begin
+            tmr_red_mode_d = TMR_UNLOAD;
+            // TODO: buffer the restoration until delay_resynch is disabled
+          end
+        end
 
-    // If error detected, do resynchronization
-    if (tmr_red_mode_q == TMR_RUN && tmr_single_mismatch_i) begin
-      $display("[ODRG] %t - mismatch detected", $realtime);
-      if (tmr_error_i[0]) tmr_incr_mismatches_o[0] = 1'b1;
-      if (tmr_error_i[1]) tmr_incr_mismatches_o[1] = 1'b1;
-      if (tmr_error_i[2]) tmr_incr_mismatches_o[2] = 1'b1;
+        // If error detected, do resynchronization
+        if (tmr_single_mismatch_i) begin
+          $display("[ODRG] %t - mismatch detected", $realtime);
+          if (tmr_error_i[0]) tmr_incr_mismatches_o[0] = 1'b1;
+          if (tmr_error_i[1]) tmr_incr_mismatches_o[1] = 1'b1;
+          if (tmr_error_i[2]) tmr_incr_mismatches_o[2] = 1'b1;
 
-      if (tmr_reg2hw.tmr_config.delay_resynch == 0) begin
-        tmr_red_mode_d = TMR_UNLOAD;
-        // TODO: buffer the restoration until delay_resynch is disabled
-      end
-    end
-
-    // If unload complete, go to reload (and reset)
-    if (tmr_red_mode_q == TMR_UNLOAD) begin
-      if (tmr_reg2hw.sp_store.q != '0) begin
-        tmr_red_mode_d = TMR_RELOAD;
-        if (tmr_reg2hw.tmr_config.setback.q) begin
-          tmr_setback_d = 1'b1;
+          if (tmr_reg2hw.tmr_config.delay_resynch == 0) begin
+            tmr_red_mode_d = TMR_UNLOAD;
+            // TODO: buffer the restoration until delay_resynch is disabled
+          end
         end
       end
-    end
 
-    // If reload complete, finish (or reset if error happens during reload)
-    if (tmr_red_mode_q == TMR_RELOAD) begin
-      if (tmr_reg2hw.sp_store.q == '0) begin
-        $display("[ODRG] %t - mismatch restored", $realtime);
-        tmr_red_mode_d = TMR_RUN;
-      end else begin
-        if ((tmr_single_mismatch_i || tmr_failure_i) && tmr_reg2hw.tmr_config.setback.q &&
-            tmr_reg2hw.tmr_config.reload_setback.q &&
-            !(tmr_reg2hw.sp_store.qe && reg_req_i.wdata == '0)) begin
-          tmr_setback_d = 1'b1;
+      TMR_UNLOAD: begin
+        // If unload complete, go to reload (and reset)
+        if (tmr_reg2hw.sp_store.q != '0) begin
+          tmr_red_mode_d = TMR_RELOAD;
+          if (tmr_reg2hw.tmr_config.setback.q) begin
+            tmr_setback_d = 1'b1;
+          end
         end
       end
-    end
 
-    // Before core startup: set TMR mode from reg2hw.mode.mode
+      TMR_RELOAD: begin
+        // If reload complete, finish (or reset if error happens during reload)
+        if (tmr_reg2hw.sp_store.q == '0) begin
+          $display("[ODRG] %t - mismatch restored", $realtime);
+          tmr_red_mode_d = TMR_RUN;
+        end else begin
+          if ((tmr_single_mismatch_i || tmr_failure_i) && tmr_reg2hw.tmr_config.setback.q &&
+              tmr_reg2hw.tmr_config.reload_setback.q &&
+              !(tmr_reg2hw.sp_store.qe && reg_req_i.wdata == '0)) begin
+            tmr_setback_d = 1'b1;
+          end
+        end
+      end
+
+      // Default: do nothing
+
+    endcase
+
+    // Logic to switch in and out of TMR
     if (!TMRFixed) begin
+      // Before core startup: set TMR mode from reg2hw.tmr_enable
       if (fetch_en_i == 0) begin
         if (tmr_reg2hw.tmr_enable.q == 1'b0) begin
           tmr_red_mode_d = NON_TMR;
@@ -151,7 +159,7 @@ module hmr_tmr_ctrl #(
           tmr_red_mode_d = TMR_RUN;
         end
       end
-      // split single-error tolerant mode to performance mode anytime (but require correct core state)
+      // split tolerant mode to performance mode anytime (but require correct core state)
       if (tmr_red_mode_q == TMR_RUN) begin
         if (tmr_reg2hw.tmr_enable.q == 1'b0) begin
           tmr_red_mode_d = NON_TMR;
