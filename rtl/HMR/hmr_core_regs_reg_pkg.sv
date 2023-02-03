417029f4dae10436e77758ca57710646b1d862af
Michael Rogenmoser <michael@rogenmoser.us>
2023-02-03 13:52:38 +0100

    Move SP store reg from tmr to core registers

--- rtl/HMR/hmr_core_regs_reg_pkg.sv
+++ rtl/HMR/hmr_core_regs_reg_pkg.sv
@@ -7,7 +7,7 @@
 package hmr_core_regs_reg_pkg;
 
   // Address widths within the block
-  parameter int BlockAw = 3;
+  parameter int BlockAw = 4;
 
   ////////////////////////////
   // Typedefs for registers //
@@ -16,6 +16,11 @@ package hmr_core_regs_reg_pkg;
   typedef struct packed {
     logic [31:0] q;
   } hmr_core_regs_reg2hw_mismatches_reg_t;
+
+  typedef struct packed {
+    logic [31:0] q;
+    logic        qe;
+  } hmr_core_regs_reg2hw_sp_store_reg_t;
 
   typedef struct packed {
     struct packed {
@@ -36,7 +41,8 @@ package hmr_core_regs_reg_pkg;
 
   // Register -> HW type
   typedef struct packed {
-    hmr_core_regs_reg2hw_mismatches_reg_t mismatches; // [31:0]
+    hmr_core_regs_reg2hw_mismatches_reg_t mismatches; // [64:33]
+    hmr_core_regs_reg2hw_sp_store_reg_t sp_store; // [32:0]
   } hmr_core_regs_reg2hw_t;
 
   // HW -> register type
@@ -46,8 +52,9 @@ package hmr_core_regs_reg_pkg;
   } hmr_core_regs_hw2reg_t;
 
   // Register offsets
-  parameter logic [BlockAw-1:0] HMR_CORE_REGS_CURRENT_MODE_OFFSET = 3'h 0;
-  parameter logic [BlockAw-1:0] HMR_CORE_REGS_MISMATCHES_OFFSET = 3'h 4;
+  parameter logic [BlockAw-1:0] HMR_CORE_REGS_CURRENT_MODE_OFFSET = 4'h 0;
+  parameter logic [BlockAw-1:0] HMR_CORE_REGS_MISMATCHES_OFFSET = 4'h 4;
+  parameter logic [BlockAw-1:0] HMR_CORE_REGS_SP_STORE_OFFSET = 4'h 8;
 
   // Reset values for hwext registers and their fields
   parameter logic [2:0] HMR_CORE_REGS_CURRENT_MODE_RESVAL = 3'h 1;
@@ -58,13 +65,15 @@ package hmr_core_regs_reg_pkg;
   // Register index
   typedef enum int {
     HMR_CORE_REGS_CURRENT_MODE,
-    HMR_CORE_REGS_MISMATCHES
+    HMR_CORE_REGS_MISMATCHES,
+    HMR_CORE_REGS_SP_STORE
   } hmr_core_regs_id_e;
 
   // Register width information to check illegal writes
-  parameter logic [3:0] HMR_CORE_REGS_PERMIT [2] = '{
+  parameter logic [3:0] HMR_CORE_REGS_PERMIT [3] = '{
     4'b 0001, // index[0] HMR_CORE_REGS_CURRENT_MODE
-    4'b 1111  // index[1] HMR_CORE_REGS_MISMATCHES
+    4'b 1111, // index[1] HMR_CORE_REGS_MISMATCHES
+    4'b 1111  // index[2] HMR_CORE_REGS_SP_STORE
   };
 
 endpackage
