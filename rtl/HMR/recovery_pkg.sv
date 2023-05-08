3fd6d902c2e6f3f9d821be393d5ca9060ca8afe6
Michael Rogenmoser <michael@rogenmoser.us>
2023-05-08 11:33:17 +0200

    Integrated ECC-protected Recovery RF.
    Making clear that the internal Floating-Point memory decoder is actually for Floating-Points.
    
    Added functions for DMR grouping.
    
    Reverted grouping functions to previous implementations
    
    Recovery Routine quite complete for RF refill.
    
    Successfully rewriting cores' RF from backup copy.
    
    Resuming cores from debug mode during recovery routine.
    
    Reverted phase of instruction lock signal.
    
    Reverted core reset into synchronous setback for clear.
    
    Recovering cores' Program Counter during the Recovery Routine.
    
    Move recovery and DMR files to HMR
    
    Change debug_rsp to debug_halted
    
    Bind branch address read from Recovery PC to assertion of internal branch detection.
    
    Fixed PC backup.
    
    Remove intruder lock
    
    Making DMR address generator start from 0.

---
+++ rtl/HMR/recovery_pkg.sv
@@ -0,0 +1,53 @@
+/* Copyright 2020 ETH Zurich and University of Bologna.
+ * Copyright and related rights are licensed under the Solderpad Hardware
+ * License, Version 0.51 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+ * or agreed to in writing, software, hardware and materials distributed under
+ * this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * Recovery Region Package
+ * 
+ */
+
+package recovery_pkg;
+
+localparam int unsigned DataWidth = 32;
+localparam int unsigned RegfileAddr = 6;
+localparam int unsigned RecoveryStateBits = 3;
+
+typedef struct packed {
+  // Write Port A
+  logic                   we_a;
+  logic [RegfileAddr-1:0] waddr_a;
+  logic [  DataWidth-1:0] wdata_a;
+  // Write Port B
+  logic                   we_b;
+  logic [RegfileAddr-1:0] waddr_b;
+  logic [  DataWidth-1:0] wdata_b;
+} regfile_write_t;
+
+typedef struct packed {
+  logic [RegfileAddr-1:0] raddr_a;
+  logic [RegfileAddr-1:0] raddr_b;
+} regfile_raddr_t;
+
+typedef struct packed {
+  logic [DataWidth-1:0] rdata_a;
+  logic [DataWidth-1:0] rdata_b;
+} regfile_rdata_t;
+
+typedef enum logic [RecoveryStateBits-1:0]{
+  IDLE       ,
+  RESET      ,
+  HALT_REQ   ,
+  HALT_WAIT  ,
+  RESTORE_PC ,
+  RESTORE_RF ,
+  RESTORE_CSR,
+  EXIT
+} recovery_routine_state_e;
+
+endpackage
