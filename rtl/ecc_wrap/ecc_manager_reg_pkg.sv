6a011b6f5acf3eca202ed847d6c64474a0bc7cb5
Michael Rogenmoser <michael@rogenmoser.us>
2023-06-22 17:56:37 +0200

    Fix ECC Manager configuration

--- rtl/ecc_wrap/ecc_manager_reg_pkg.sv
+++ rtl/ecc_wrap/ecc_manager_reg_pkg.sv
@@ -6,11 +6,8 @@
 
 package ecc_manager_reg_pkg;
 
-  // Param list
-  parameter int NumBanks = 6;
-
   // Address widths within the block
-  parameter int BlockAw = 7;
+  parameter int BlockAw = 5;
 
   ////////////////////////////
   // Typedefs for registers //
@@ -18,7 +15,7 @@ package ecc_manager_reg_pkg;
 
   typedef struct packed {
     logic [31:0] q;
-  } ecc_manager_reg2hw_mismatch_count_mreg_t;
+  } ecc_manager_reg2hw_mismatch_count_reg_t;
 
   typedef struct packed {
     logic [31:0] q;
@@ -26,141 +23,78 @@ package ecc_manager_reg_pkg;
 
   typedef struct packed {
     logic [31:0] q;
-  } ecc_manager_reg2hw_scrub_fix_count_mreg_t;
+  } ecc_manager_reg2hw_scrub_fix_count_reg_t;
 
   typedef struct packed {
     logic [31:0] q;
-  } ecc_manager_reg2hw_scrub_uncorrectable_count_mreg_t;
+  } ecc_manager_reg2hw_scrub_uncorrectable_count_reg_t;
 
   typedef struct packed {
     logic [31:0] q;
-  } ecc_manager_reg2hw_write_mask_data_n_mreg_t;
+  } ecc_manager_reg2hw_write_mask_data_n_reg_t;
 
   typedef struct packed {
     logic [6:0]  q;
-  } ecc_manager_reg2hw_write_mask_ecc_n_mreg_t;
+  } ecc_manager_reg2hw_write_mask_ecc_n_reg_t;
 
   typedef struct packed {
     logic [31:0] d;
     logic        de;
-  } ecc_manager_hw2reg_mismatch_count_mreg_t;
+  } ecc_manager_hw2reg_mismatch_count_reg_t;
 
   typedef struct packed {
     logic [31:0] d;
     logic        de;
-  } ecc_manager_hw2reg_scrub_fix_count_mreg_t;
+  } ecc_manager_hw2reg_scrub_fix_count_reg_t;
 
   typedef struct packed {
     logic [31:0] d;
     logic        de;
-  } ecc_manager_hw2reg_scrub_uncorrectable_count_mreg_t;
+  } ecc_manager_hw2reg_scrub_uncorrectable_count_reg_t;
 
   // Register -> HW type
   typedef struct packed {
-    ecc_manager_reg2hw_mismatch_count_mreg_t [5:0] mismatch_count; // [841:650]
-    ecc_manager_reg2hw_scrub_interval_reg_t scrub_interval; // [649:618]
-    ecc_manager_reg2hw_scrub_fix_count_mreg_t [5:0] scrub_fix_count; // [617:426]
-    ecc_manager_reg2hw_scrub_uncorrectable_count_mreg_t [5:0] scrub_uncorrectable_count; // [425:234]
-    ecc_manager_reg2hw_write_mask_data_n_mreg_t [5:0] write_mask_data_n; // [233:42]
-    ecc_manager_reg2hw_write_mask_ecc_n_mreg_t [5:0] write_mask_ecc_n; // [41:0]
+    ecc_manager_reg2hw_mismatch_count_reg_t mismatch_count; // [166:135]
+    ecc_manager_reg2hw_scrub_interval_reg_t scrub_interval; // [134:103]
+    ecc_manager_reg2hw_scrub_fix_count_reg_t scrub_fix_count; // [102:71]
+    ecc_manager_reg2hw_scrub_uncorrectable_count_reg_t scrub_uncorrectable_count; // [70:39]
+    ecc_manager_reg2hw_write_mask_data_n_reg_t write_mask_data_n; // [38:7]
+    ecc_manager_reg2hw_write_mask_ecc_n_reg_t write_mask_ecc_n; // [6:0]
   } ecc_manager_reg2hw_t;
 
   // HW -> register type
   typedef struct packed {
-    ecc_manager_hw2reg_mismatch_count_mreg_t [5:0] mismatch_count; // [593:396]
-    ecc_manager_hw2reg_scrub_fix_count_mreg_t [5:0] scrub_fix_count; // [395:198]
-    ecc_manager_hw2reg_scrub_uncorrectable_count_mreg_t [5:0] scrub_uncorrectable_count; // [197:0]
+    ecc_manager_hw2reg_mismatch_count_reg_t mismatch_count; // [98:66]
+    ecc_manager_hw2reg_scrub_fix_count_reg_t scrub_fix_count; // [65:33]
+    ecc_manager_hw2reg_scrub_uncorrectable_count_reg_t scrub_uncorrectable_count; // [32:0]
   } ecc_manager_hw2reg_t;
 
   // Register offsets
-  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_0_OFFSET = 7'h 0;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_1_OFFSET = 7'h 4;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_2_OFFSET = 7'h 8;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_3_OFFSET = 7'h c;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_4_OFFSET = 7'h 10;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_5_OFFSET = 7'h 14;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_INTERVAL_OFFSET = 7'h 18;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_0_OFFSET = 7'h 1c;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_1_OFFSET = 7'h 20;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_2_OFFSET = 7'h 24;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_3_OFFSET = 7'h 28;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_4_OFFSET = 7'h 2c;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_5_OFFSET = 7'h 30;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_0_OFFSET = 7'h 34;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_1_OFFSET = 7'h 38;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_2_OFFSET = 7'h 3c;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_3_OFFSET = 7'h 40;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_4_OFFSET = 7'h 44;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_5_OFFSET = 7'h 48;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_0_OFFSET = 7'h 4c;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_1_OFFSET = 7'h 50;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_2_OFFSET = 7'h 54;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_3_OFFSET = 7'h 58;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_4_OFFSET = 7'h 5c;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_5_OFFSET = 7'h 60;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_ECC_N_0_OFFSET = 7'h 64;
-  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_ECC_N_1_OFFSET = 7'h 68;
+  parameter logic [BlockAw-1:0] ECC_MANAGER_MISMATCH_COUNT_OFFSET = 5'h 0;
+  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_INTERVAL_OFFSET = 5'h 4;
+  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_FIX_COUNT_OFFSET = 5'h 8;
+  parameter logic [BlockAw-1:0] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_OFFSET = 5'h c;
+  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_DATA_N_OFFSET = 5'h 10;
+  parameter logic [BlockAw-1:0] ECC_MANAGER_WRITE_MASK_ECC_N_OFFSET = 5'h 14;
 
   // Register index
   typedef enum int {
-    ECC_MANAGER_MISMATCH_COUNT_0,
-    ECC_MANAGER_MISMATCH_COUNT_1,
-    ECC_MANAGER_MISMATCH_COUNT_2,
-    ECC_MANAGER_MISMATCH_COUNT_3,
-    ECC_MANAGER_MISMATCH_COUNT_4,
-    ECC_MANAGER_MISMATCH_COUNT_5,
+    ECC_MANAGER_MISMATCH_COUNT,
     ECC_MANAGER_SCRUB_INTERVAL,
-    ECC_MANAGER_SCRUB_FIX_COUNT_0,
-    ECC_MANAGER_SCRUB_FIX_COUNT_1,
-    ECC_MANAGER_SCRUB_FIX_COUNT_2,
-    ECC_MANAGER_SCRUB_FIX_COUNT_3,
-    ECC_MANAGER_SCRUB_FIX_COUNT_4,
-    ECC_MANAGER_SCRUB_FIX_COUNT_5,
-    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_0,
-    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_1,
-    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_2,
-    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_3,
-    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_4,
-    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_5,
-    ECC_MANAGER_WRITE_MASK_DATA_N_0,
-    ECC_MANAGER_WRITE_MASK_DATA_N_1,
-    ECC_MANAGER_WRITE_MASK_DATA_N_2,
-    ECC_MANAGER_WRITE_MASK_DATA_N_3,
-    ECC_MANAGER_WRITE_MASK_DATA_N_4,
-    ECC_MANAGER_WRITE_MASK_DATA_N_5,
-    ECC_MANAGER_WRITE_MASK_ECC_N_0,
-    ECC_MANAGER_WRITE_MASK_ECC_N_1
+    ECC_MANAGER_SCRUB_FIX_COUNT,
+    ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT,
+    ECC_MANAGER_WRITE_MASK_DATA_N,
+    ECC_MANAGER_WRITE_MASK_ECC_N
   } ecc_manager_id_e;
 
   // Register width information to check illegal writes
-  parameter logic [3:0] ECC_MANAGER_PERMIT [27] = '{
-    4'b 1111, // index[ 0] ECC_MANAGER_MISMATCH_COUNT_0
-    4'b 1111, // index[ 1] ECC_MANAGER_MISMATCH_COUNT_1
-    4'b 1111, // index[ 2] ECC_MANAGER_MISMATCH_COUNT_2
-    4'b 1111, // index[ 3] ECC_MANAGER_MISMATCH_COUNT_3
-    4'b 1111, // index[ 4] ECC_MANAGER_MISMATCH_COUNT_4
-    4'b 1111, // index[ 5] ECC_MANAGER_MISMATCH_COUNT_5
-    4'b 1111, // index[ 6] ECC_MANAGER_SCRUB_INTERVAL
-    4'b 1111, // index[ 7] ECC_MANAGER_SCRUB_FIX_COUNT_0
-    4'b 1111, // index[ 8] ECC_MANAGER_SCRUB_FIX_COUNT_1
-    4'b 1111, // index[ 9] ECC_MANAGER_SCRUB_FIX_COUNT_2
-    4'b 1111, // index[10] ECC_MANAGER_SCRUB_FIX_COUNT_3
-    4'b 1111, // index[11] ECC_MANAGER_SCRUB_FIX_COUNT_4
-    4'b 1111, // index[12] ECC_MANAGER_SCRUB_FIX_COUNT_5
-    4'b 1111, // index[13] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_0
-    4'b 1111, // index[14] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_1
-    4'b 1111, // index[15] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_2
-    4'b 1111, // index[16] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_3
-    4'b 1111, // index[17] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_4
-    4'b 1111, // index[18] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT_5
-    4'b 1111, // index[19] ECC_MANAGER_WRITE_MASK_DATA_N_0
-    4'b 1111, // index[20] ECC_MANAGER_WRITE_MASK_DATA_N_1
-    4'b 1111, // index[21] ECC_MANAGER_WRITE_MASK_DATA_N_2
-    4'b 1111, // index[22] ECC_MANAGER_WRITE_MASK_DATA_N_3
-    4'b 1111, // index[23] ECC_MANAGER_WRITE_MASK_DATA_N_4
-    4'b 1111, // index[24] ECC_MANAGER_WRITE_MASK_DATA_N_5
-    4'b 1111, // index[25] ECC_MANAGER_WRITE_MASK_ECC_N_0
-    4'b 0011  // index[26] ECC_MANAGER_WRITE_MASK_ECC_N_1
+  parameter logic [3:0] ECC_MANAGER_PERMIT [6] = '{
+    4'b 1111, // index[0] ECC_MANAGER_MISMATCH_COUNT
+    4'b 1111, // index[1] ECC_MANAGER_SCRUB_INTERVAL
+    4'b 1111, // index[2] ECC_MANAGER_SCRUB_FIX_COUNT
+    4'b 1111, // index[3] ECC_MANAGER_SCRUB_UNCORRECTABLE_COUNT
+    4'b 1111, // index[4] ECC_MANAGER_WRITE_MASK_DATA_N
+    4'b 0001  // index[5] ECC_MANAGER_WRITE_MASK_ECC_N
   };
 
 endpackage
