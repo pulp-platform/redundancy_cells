d83113e588780f827d109c1296ea76a9c92394c9
Michael Rogenmoser <michael@rogenmoser.us>
2024-01-10 16:09:36 +0100

    HMR: Add initial wrapper structure
    HMR: Add ODRG & TCLS support
    
    HMR: add initial configuration regfile
    
    HMR: additional registers
    
    Add files, initial fixes
    
    Add registers for tmr, modify and connect config
    
    Clarify core interleaving with functions, fixes
    
    Clean up
    
    Changed HMR interface's core grouping parameters to localparam as they depend on other parameters. Removed dependency from red_mode_q in generate statements.
    
    Indentation :art:
    
    Having couples of cores with the same inputs and generating the same outputs.
    
    Integrated DMR Core Checker.
    
    Use functions for DMR core selection
    
    Reposition TMR registers and FSM
    
    Externalize TMR control from HMR
    
    Add DMR configuration registers
    
    HMR: Add C include files
    
    Fix signal assignment in TMR mode

---
+++ rtl/HMR/hmr_core.h
@@ -0,0 +1,25 @@
+// Generated register defines for HMR_core_regs
+
+#ifndef _HMR_CORE_REGS_REG_DEFS_
+#define _HMR_CORE_REGS_REG_DEFS_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// Register width
+#define HMR_CORE_REGS_PARAM_REG_WIDTH 32
+
+// Value to determine wich redundancy mode the core with that ID is in.
+#define HMR_CORE_REGS_CURRENT_MODE_REG_OFFSET 0x0
+#define HMR_CORE_REGS_CURRENT_MODE_INDEPENDENT_BIT 0
+#define HMR_CORE_REGS_CURRENT_MODE_DUAL_BIT 1
+#define HMR_CORE_REGS_CURRENT_MODE_TRIPLE_BIT 2
+
+// Mismatches of the core
+#define HMR_CORE_REGS_MISMATCHES_REG_OFFSET 0x4
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif  // _HMR_CORE_REGS_REG_DEFS_
+// End generated register defines for HMR_core_regs
