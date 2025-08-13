fde1ffef66d625497acf8114cd32bfd464dfbfd9
Michael Rogenmoser <michael@rogenmoser.us>
2025-08-13 16:33:48 +0200

    Convert HMR registers to systemrdl w/ APB interface

--- rtl/HMR/hmr_dmr_ctrl.sv
+++ rtl/HMR/hmr_dmr_ctrl.sv
@@ -18,16 +18,16 @@ module hmr_dmr_ctrl
   parameter bit  DMRFixed       = 1'b0,
   parameter bit  DefaultInDMR   = DMRFixed ? 1'b1 : 1'b0,
   parameter bit  RapidRecovery  = 1'b0,
-  parameter type reg_req_t      = logic,
-  parameter type reg_resp_t     = logic
+  parameter type apb_req_t      = logic,
+  parameter type apb_resp_t     = logic
 ) (
   input  logic       clk_i,
   input  logic       rst_ni,
   // input  logic       test_enable_i,
 
   // Register interface
-  input  reg_req_t   reg_req_i,
-  output reg_resp_t  reg_resp_o,
+  input  apb_req_t   reg_req_i,
+  output apb_resp_t  reg_resp_o,
 
   // CTRL from external (e.g. HMR ctrl regs)
   input  logic       dmr_enable_q_i,
@@ -60,39 +60,43 @@ module hmr_dmr_ctrl
   typedef enum logic [2:0] {NON_DMR, DMR_RUN, DMR_RESTORE} dmr_mode_e;
   localparam dmr_mode_e DefaultDMRMode = DefaultInDMR || DMRFixed ? DMR_RUN : NON_DMR;
 
-  hmr_dmr_regs_reg_pkg::hmr_dmr_regs_reg2hw_t dmr_reg2hw;
-  hmr_dmr_regs_reg_pkg::hmr_dmr_regs_hw2reg_t dmr_hw2reg;
+  hmr_dmr_regs_reg_pkg::hmr_dmr__out_t dmr_reg2hw;
+  hmr_dmr_regs_reg_pkg::hmr_dmr__in_t dmr_hw2reg;
 
   dmr_mode_e dmr_red_mode_d, dmr_red_mode_q;
 
   assign grp_in_independent_o = dmr_red_mode_q == NON_DMR;
-  assign rapid_recovery_en_o = dmr_reg2hw.dmr_config.rapid_recovery.q && RapidRecovery;
+  assign rapid_recovery_en_o = dmr_reg2hw.dmr_config.rapid_recovery.value && RapidRecovery;
 
   assign sw_synch_req_o = synch_req & ~synch_req_sent_q;
   assign synch_req_sent_d = synch_req;
   assign sw_resynch_req_o = resynch_req & ~resynch_req_sent_q;
   assign resynch_req_sent_d = resynch_req;
-  assign checkpoint_o = dmr_reg2hw.checkpoint_addr.q;
+  assign checkpoint_o = dmr_reg2hw.checkpoint_addr.checkpoint_addr.value;
 
-  hmr_dmr_regs_reg_top #(
-    .reg_req_t(reg_req_t),
-    .reg_rsp_t(reg_resp_t)
-  ) i_dmr_regs (
-    .clk_i,
-    .rst_ni,
-    .reg_req_i(reg_req_i),
-    .reg_rsp_o(reg_resp_o),
-    .reg2hw   (dmr_reg2hw),
-    .hw2reg   (dmr_hw2reg),
-    .devmode_i('0)
+  hmr_dmr_regs_reg_top i_dmr_regs (
+    .clk(clk_i),
+    .arst_n(rst_ni),
+    .s_apb_psel(reg_req_i.psel),
+    .s_apb_penable(apb_req_i.penable),
+    .s_apb_pwrite(apb_req_i.pwrite),
+    .s_apb_pprot(apb_req_i.pprot),
+    .s_apb_paddr(apb_req_i.paddr[3:0]),
+    .s_apb_pwdata(apb_req_i.pwdata),
+    .s_apb_pstrb(apb_req_i.pstrb),
+    .s_apb_pready(apb_resp_o.pready),
+    .s_apb_prdata(apb_resp_o.prdata),
+    .s_apb_pslverr(apb_resp_o.pslverr),
+    .hwif_in(dmr_hw2reg),
+    .hwif_out(dmr_reg2hw)
   );
 
   // Global config update
-  assign dmr_hw2reg.dmr_enable.de = dmr_enable_qe_i;
-  assign dmr_hw2reg.dmr_enable.d  = dmr_enable_q_i;
-  assign dmr_hw2reg.dmr_config.rapid_recovery.de = rapid_recovery_qe_i || ~RapidRecovery;
-  assign dmr_hw2reg.dmr_config.rapid_recovery.d  = rapid_recovery_q_i && RapidRecovery;
-  assign dmr_hw2reg.dmr_config.force_recovery.d  = force_recovery_qe_i ? force_recovery_q_i : 1'b0;
+  assign dmr_hw2reg.dmr_enable.dmr_enable.we       = dmr_enable_qe_i;
+  assign dmr_hw2reg.dmr_enable.dmr_enable.next     = dmr_enable_q_i;
+  assign dmr_hw2reg.dmr_config.rapid_recovery.we   = rapid_recovery_qe_i || ~RapidRecovery;
+  assign dmr_hw2reg.dmr_config.rapid_recovery.next = rapid_recovery_q_i && RapidRecovery;
+  assign dmr_hw2reg.dmr_config.force_recovery.next = force_recovery_qe_i ? force_recovery_q_i : 1'b0;
 
   /**************************
    *  FSM for DMR lockstep  *
@@ -106,24 +110,24 @@ module hmr_dmr_ctrl
     resynch_req = 1'b0;
     synch_req = 1'b0;
 
-    dmr_hw2reg.dmr_config.force_recovery.de = force_recovery_qe_i;
+    dmr_hw2reg.dmr_config.force_recovery.we = force_recovery_qe_i;
 
     case (dmr_red_mode_q)
       DMR_RUN: begin
         // If forced execute recovery
-        if (dmr_reg2hw.dmr_config.force_recovery.q && RapidRecovery &&
-            dmr_reg2hw.dmr_config.rapid_recovery.q) begin
-          dmr_hw2reg.dmr_config.force_recovery.de = 1'b1;
+        if (dmr_reg2hw.dmr_config.force_recovery.value && RapidRecovery &&
+            dmr_reg2hw.dmr_config.rapid_recovery.value) begin
+          dmr_hw2reg.dmr_config.force_recovery.we = 1'b1;
           dmr_red_mode_d = DMR_RESTORE;
         end
 
         // If error detected, restore
-        if (dmr_error_i && RapidRecovery && dmr_reg2hw.dmr_config.rapid_recovery.q) begin
+        if (dmr_error_i && RapidRecovery && dmr_reg2hw.dmr_config.rapid_recovery.value) begin
           $display("[HMR-dual] %t - mismatch detected, rapid recovery starting", $realtime);
           dmr_red_mode_d = DMR_RESTORE;
         end
 
-        if (dmr_error_i && (!RapidRecovery || !dmr_reg2hw.dmr_config.rapid_recovery.q)) begin
+        if (dmr_error_i && (!RapidRecovery || !dmr_reg2hw.dmr_config.rapid_recovery.value)) begin
           $display("[HMR-dual] %t - mismatch detected, SW trigger", $realtime);
           resynch_req = 1'b1;
         end
@@ -144,10 +148,10 @@ module hmr_dmr_ctrl
     // Logic to switch in and out of DMR
     if (!DMRFixed) begin
       // Set DMR mode on external signal that cores are synchronized
-      if (dmr_red_mode_q == NON_DMR && dmr_reg2hw.dmr_enable.q == 1'b1) begin
+      if (dmr_red_mode_q == NON_DMR && dmr_reg2hw.dmr_enable.dmr_enable.value == 1'b1) begin
         synch_req = 1'b1;
         if (cores_synch_q == 1'b1) begin
-          if (dmr_reg2hw.dmr_config.rapid_recovery.q == 1'b1) begin
+          if (dmr_reg2hw.dmr_config.rapid_recovery.value == 1'b1) begin
             dmr_red_mode_d = DMR_RESTORE;
           end else begin
             dmr_red_mode_d = DMR_RUN;
@@ -157,7 +161,7 @@ module hmr_dmr_ctrl
       end
       // Before core startup: set DMR mode from reg2hw.dmr_enable
       if (fetch_en_i == 0) begin
-        if (dmr_reg2hw.dmr_enable.q == 1'b0) begin
+        if (dmr_reg2hw.dmr_enable.dmr_enable.value == 1'b0) begin
           dmr_red_mode_d = NON_DMR;
         end else begin
           synch_req = 1'b0;
@@ -166,7 +170,7 @@ module hmr_dmr_ctrl
       end
       // split tolerant mode to performance mode anytime (but require correct core state)
       if (dmr_red_mode_q == DMR_RUN) begin
-        if (dmr_reg2hw.dmr_enable.q == 1'b0) begin
+        if (dmr_reg2hw.dmr_enable.dmr_enable.value == 1'b0) begin
           dmr_red_mode_d = NON_DMR;
           setback_o = 2'b10;
         end
