9cf8f3dfd2f90f25380c28cfbbfdc93b109c74ac
Chaoqun Liang <chaoqun.liang@unibo.it>
2026-05-11 15:42:15 +0200

    add tmr status into rel counter

--- rtl/rel_counter.sv
+++ rtl/rel_counter.sv
@@ -12,28 +12,46 @@
 
 module rel_counter #(
     parameter int unsigned WIDTH = 4,
-    parameter bit STICKY_OVERFLOW = 1'b0
+    parameter bit STICKY_OVERFLOW = 1'b0,
+    /// Status and control signals are triplicated
+    parameter bit          TmrStatus       = 1'b0,
+    /// DO NOT OVERRIDE
+    parameter int unsigned HsWidth         = TmrStatus ? 3 : 1
 )(
     input  logic                  clk_i,
     input  logic                  rst_ni,
-    input  logic                  clear_i, // synchronous clear
-    input  logic                  en_i,    // enable the counter
-    input  logic                  load_i,  // load a new value
-    input  logic                  down_i,  // downcount, default is up
+    input  logic [HsWidth-1:0]    clear_i, // synchronous clear
+    input  logic [HsWidth-1:0]    en_i,    // enable the counter
+    input  logic [HsWidth-1:0]    load_i,  // load a new value
+    input  logic [HsWidth-1:0]    down_i,  // downcount, default is up
     input  logic [WIDTH-1:0]      d_i,
     output logic [2:0][WIDTH-1:0] q_o,
     output logic [2:0]            overflow_o
-);
+);  
+
+    logic [2:0] clear, en, load, down;
+    if (TmrStatus) begin : gen_tmr_inputs
+        assign clear = clear_i;
+        assign en    = en_i;
+        assign load  = load_i;
+        assign down  = down_i;
+    end else begin : gen_broadcast_inputs
+        assign clear = {3{clear_i}};
+        assign en    = {3{en_i}};
+        assign load  = {3{load_i}};
+        assign down  = {3{down_i}};
+    end
+
     rel_delta_counter #(
         .WIDTH          (WIDTH),
         .STICKY_OVERFLOW (STICKY_OVERFLOW)
     ) i_counter (
         .clk_i,
         .rst_ni,
-        .clear_i,
-        .en_i,
-        .load_i,
-        .down_i,
+        .clear_i(clear),
+        .en_i(en),
+        .load_i(load),
+        .down_i(down),
         .delta_i({{WIDTH-1{1'b0}}, 1'b1}),
         .d_i,
         .q_o,
